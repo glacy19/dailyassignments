50f340d5278481b72c3ab868d896a3b81f031c9c
genesislacy <gcl61019@gmail.com>
2025-03-02 18:18:44 -0700

    c

--- day -09 and 10.R/Daily Exercise 9 and 10.R
+++ day -09 and 10.R/Daily Exercise 9 and 10.R
@@ -8,4 +8,20 @@ model <- lm(Ozone ~ Solar.R, data = airquality)
 summary(model)
 plot(model)
 
+## I think this is a valid model because the p-value is 0.0001793 and the R^2 value is 0.1213. 
+## The R^2 valued shows that 12% of the variability in the ozone levels can be further explained by solar radiation. 
 
+library(broom)
+a <- augment(model, data = airquality)
+
+library(ggplot2)
+ggplot(a, aes(x = Ozone, y = .fitted)) +
+  geom_point() + 
+  geom_abline(intercept = 0, slope = 1, color = "red") +
+  labs(
+    title = "Actual VS Predicted Ozone Levels",
+    x = "Actual Ozone",
+    y = "Predicted Ozone",
+    subtitle = paste("Correlation:", round(cor(a$Ozone, a$.fitted), 2))
+  ) +
+  theme_minimal()
