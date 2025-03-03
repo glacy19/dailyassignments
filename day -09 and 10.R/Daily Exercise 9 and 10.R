cddb6473683113e1bf3fc06865e5ed95d73b8953
genesislacy <gcl61019@gmail.com>
2025-03-02 17:08:23 -0700

    c

--- day -09 and 10.R/Daily Exercise 9 and 10.R
+++ day -09 and 10.R/Daily Exercise 9 and 10.R
@@ -1,2 +1,9 @@
 airquality
-visdat(airquality)
+vis_dat(airquality)
+airquality <- na.omit(airquality)
+model <- lm(Ozone ~ Solar.R, data = airquality)
+
+## I chose Solar. R because the ozone/ atmosphere is relative to the solar radiation that the earth holds in(?). 
+
+summary(model)
+
