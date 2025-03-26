a8c138b68b42bd9e1eb063751d4cf59ea82c20a3
genesislacy <gcl61019@gmail.com>
2025-03-26 02:47:21 -0600

    c

--- day-15.R/Data Training.R
+++ day-15.R/Data Training.R
@@ -1,3 +1,15 @@
 library(tidymodels)
-palmerpenguins
+library(palmerpenguins)
+
+set.seed(123)
+
+(penguins_split <- initial_split(drop_na(penguins), prop = 0.7, strata = species))
+#> <Training/Testing/Total>
+#> <265/68/333>
+penguins_train  <- training(penguins_split)
+penguins_test   <- testing(penguins_split)
+
+penguin_folds <- vfold_cv(penguins_train, v = 10)
+
+penguins_folds
 
