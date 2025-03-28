f1824f30c331ad7a6ac3123b0dc77c59821a70b8
genesislacy <gcl61019@gmail.com>
2025-03-28 13:04:57 -0600

    c

--- day-15.R/Data Training.R
+++ day-15.R/Data Training.R
@@ -14,3 +14,33 @@ penguin_folds <- vfold_cv(penguins_train, v = 10)
 
 print(penguin_folds)
 
+# defining multinominal logistic regression model, there are 3 species types
+
+multinom_model <- multinom_reg() %>%
+  set_engine("nnet") %>%
+  set_mode("classification")
+
+# using default model which is nnet
+rand_forest_model <- rand_forest() %>%
+  set_engine("ranger") %>%
+  set_mode("classification")
+
+penguins_wf_Set <- workflow_set(
+  preproc = list(species ~ .),
+  models = list(multinom = multinom_model, rf = rand_forest_model))
+
+# fit both models with the 10 fold cross validation, #fit_resamples is a function from tidymodels ecosystem, specicially in the workflows
+
+penguins_res <- penguins_wf_Set %>%
+  workflow_map("fit_resamples",
+               resamples = penguin_folds, control = control_resamples(save_pred = TRUE))
+
+# saving metrics
+penguins_res_metrics <- collect_metrics(penguins_res)
+
+
+# compare
+accuracy_comparison <- penguins_res_metrics %>%
+  filter(.metric == "accuracy")
+
+print(accuracy_comparison)
